16dbdd650898fe82e994299836e49e11260281f8
Livia <livinha.guimaraes.013@gmail.com>
2021-08-12 23:41:20 -0300

    initial setup

---
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -0,0 +1,18 @@
+package com.algaworks.algalog.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {		
+		return null;
+	}
+
+}
